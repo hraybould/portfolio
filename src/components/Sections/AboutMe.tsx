9a4dd4602b081db19e46dd94adf90c81c5c5d9a8
Harrison Raybould <h_c_raybould@hotmail.com>
2022-11-09 09:01:14 +0000

    adjusted text formatting and clipped img

--- src/components/Sections/AboutMe.tsx
+++ src/components/Sections/AboutMe.tsx
@@ -3,21 +3,30 @@ import wave from "assets/memoji/wave.png";
 
 interface AboutMeProps {}
 
-const DateOfBirth = new Date("1994-12-16");
+const DATE_OF_BIRTH = new Date("1994-12-16");
+
+const ClosingTag = () => <span className="ClosingTag">&#x20;&#x2F;&#x3E;</span>;
 
 export const AboutMe: React.FC<AboutMeProps> = () => {
   return (
     <div className="AboutMeSection">
-      <h3 className="HiText LargeText CursiveFont">Hi</h3>
-      <img src={wave} alt="Memoji of Me, Harrison" />
+      <h1 className="HiText CursiveFont">Hi</h1>
+      <img className="ClippedImg" src={wave} alt="Memoji of Me, Harrison" />
       <div className="DisplayFlex">
-        <div className="ImText CursiveFont LargeText">I'm</div>
+        <div className="ImText CursiveFont LargeText">I'm &#8230;</div>
         <div>
-          <h3>Harrison</h3>
-          <div>
-            {durationFormatter({ start: DateOfBirth, format: ["years"] })} old
-          </div>
-          <div>a UK-based Web Developer</div>
+          <h3 className="InfoText">
+            Harrison
+            <ClosingTag />
+          </h3>
+          <h3 className="InfoText">
+            {durationFormatter({ start: DATE_OF_BIRTH, format: ["years"] })} old
+            <ClosingTag />
+          </h3>
+          <h3 className="InfoText">
+            a UK-based Web Developer
+            <ClosingTag />
+          </h3>
         </div>
       </div>
     </div>
