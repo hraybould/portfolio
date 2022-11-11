331805bff061c8922788da3c97c9e41dbd79c918
Harrison Raybould <h_c_raybould@hotmail.com>
2022-11-11 21:31:42 +0000

    added to Contact info with react-icons

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,3 +1,6 @@
+import { BsGithub, BsStackOverflow, BsLinkedin } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+
 const emailAddres = "h_c_raybould@hotmail.com";
 const emailSubject = "Hi";
 const emailBody = `Hi Harrison,%0D%0A%0D%0AI just came across your website and I wanted to you regarding`;
@@ -9,19 +12,64 @@ interface ContactProps {}
 
 export const Contact: React.FC<ContactProps> = () => {
   return (
-    <div className="ContactSection">
-      <div className="DisplayFlex JustifySpaceBetween">
+    <div className="ContactSection DisplayFlex FlexColumn">
+      <div className="DisplayFlex FlexRow SmallGap JustifySpaceBetween FullWidth">
         <div>Feel free to get in touch:</div>
-        <div className="TextLink">
-          <a className="Link ContactLink" href={emailLinkFull}>
+        <div className="TextLink ContactLink">
+          <a className="Link" href={emailLinkFull}>
             Contact Me
           </a>
         </div>
       </div>
 
-      <div className="DisplayFlex JustifySpaceBetween">
-        <div>Email:</div>
+      <div className="DisplayFlex FlexRow SmallGap JustifySpaceBetween FullWidth">
+        <div className="DisplayFlex SmallGap">
+          <AiOutlineMail />
+          Email:
+        </div>
         <div className="TextLink">{emailAddres}</div>
+      </div>
+
+      <div className="DisplayFlex FlexRow SmallGap JustifySpaceBetween FullWidth">
+        <div className="DisplayFlex SmallGap">
+          <BsGithub />
+          GitHub:
+        </div>
+        <div>
+          <a className="Link" href="https://github.com/hraybould">
+            harrisonr
+          </a>
+        </div>
+      </div>
+
+      <div className="DisplayFlex FlexRow SmallGap JustifySpaceBetween FullWidth">
+        <div className="DisplayFlex SmallGap">
+          <BsStackOverflow />
+          Stack Overflow:
+        </div>
+        <div>
+          <a
+            className="Link"
+            href="https://stackoverflow.com/users/15291770/harrison"
+          >
+            harrison
+          </a>
+        </div>
+      </div>
+
+      <div className="DisplayFlex FlexRow SmallGap JustifySpaceBetween FullWidth">
+        <div className="DisplayFlex SmallGap">
+          <BsLinkedin />
+          LinkedIn:
+        </div>
+        <div>
+          <a
+            className="Link"
+            href="https://www.linkedin.com/in/harrison-raybould/"
+          >
+            harrison-raybould
+          </a>
+        </div>
       </div>
     </div>
   );
