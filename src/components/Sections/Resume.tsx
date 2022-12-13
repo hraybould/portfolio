078f01bab956d152d593041b3b84cbe0562c1317
Harrison Raybould <h_c_raybould@hotmail.com>
2022-12-13 22:48:17 +0000

    added further details to past roles

--- src/components/Sections/Resume.tsx
+++ src/components/Sections/Resume.tsx
@@ -5,6 +5,7 @@ import {
   STARTED_MTC,
   STARTED_STORMFRONT_RETAIL,
 } from "./keyDates";
+import { Link } from "components/Link";
 
 interface ResumeProps {}
 
@@ -28,6 +29,7 @@ export const Resume: React.FC<ResumeProps> = () => {
 interface ResumeHeadingProps {
   heading: string;
   subTitle?: string;
+  link?: string;
   start: Date;
   end: Date;
   smaller?: boolean;
@@ -36,29 +38,28 @@ interface ResumeHeadingProps {
 const ResumeHeading: React.FC<ResumeHeadingProps> = ({
   heading,
   subTitle,
+  link,
   start,
   end,
   smaller = false,
 }) => {
   const headingContent = (
     <>
-      {heading}
-      {subTitle && <em>&nbsp;({subTitle})</em>}
+      {link ? <Link href={link}>{heading}</Link> : heading}
+      {subTitle && <em className="XSmallText Italics">&nbsp;({subTitle})</em>}
+    </>
+  );
+
+  const datesContent = (
+    <>
+      {format(start, "MMM yyyy")}&nbsp;-&nbsp;{format(end, "MMM yyyy")}
     </>
   );
 
   return (
     <div className="DisplayFlex JustifySpaceBetween">
-      {smaller ? (
-        <h5>{headingContent}</h5>
-      ) : (
-        <h4 className="SectionHeading">{headingContent}</h4>
-      )}
-
-      <div>
-        {format(start, "MMMM yyyy")}&nbsp;-&nbsp;
-        {format(end, "MMMM yyyy")}
-      </div>
+      {smaller ? <div>{headingContent}</div> : <h5>{headingContent}</h5>}
+      {smaller ? <div>{datesContent}</div> : <h5>{datesContent}</h5>}
     </div>
   );
 };
@@ -86,74 +87,104 @@ const PersonalProfile: React.FC = () => (
 // Key Achievements - START
 
 type KeyAchievement = {
-  achievement: string;
+  achievement: React.ReactNode;
   subAchievements?: KeyAchievement[];
   visible: boolean;
 };
 const KEY_ACHIEVEMENTS: KeyAchievement[] = [
   {
-    achievement:
-      "Rewrote an existing client project from type-less JavaScript to TypeScript with extensive Types and Hooks",
+    achievement: (
+      <>
+        Rewrote an existing client project from JavaScript to TypeScript with
+        extensive Types and Hooks
+      </>
+    ),
     visible: true,
   },
   {
-    achievement:
-      "Represented The Manufacturing Technology Centre (MTC) at the International Conference on Additive Manufacturing, EMO Hannover 2019",
+    achievement: (
+      <>
+        Represented The Manufacturing Technology Centre (MTC) at the
+        International Conference on Additive Manufacturing, EMO Hannover 2019
+      </>
+    ),
     visible: true,
   },
   {
-    achievement:
-      "Lead development in key areas of projects in areas such as: Additive Manufacturing (AM) and low-cost Industry 4.0 condition monitoring.",
+    achievement: (
+      <>
+        Lead development in key areas of projects in areas such as: Additive
+        Manufacturing (AM) and low-cost Industry 4.0 condition monitoring.
+      </>
+    ),
     subAchievements: [
       {
-        achievement:
-          "Obtained Level 1 Vibration Analyst from RMS Solutions</li>",
-        visible: true,
+        achievement: <>Obtained Level 1 Vibration Analyst from RMS Solutions</>,
+        visible: false,
       },
       {
-        achievement:
-          "Early completion of The MTC Graduate Program, transferring from the 2-year scheme within the first 6 months",
-        visible: true,
+        achievement: (
+          <>
+            Early completion of The MTC Graduate Program, transferring from the
+            2-year scheme within the first 6 months
+          </>
+        ),
+        visible: false,
       },
     ],
     visible: true,
   },
   {
-    achievement:
-      "Graduated from Exeter University with a 2:1 BSc Physics (Hons.)",
+    achievement: (
+      <>
+        Halved the time in which a repair could be processed and completed as a
+        Senior Technician at Stormfront Retail
+      </>
+    ),
     visible: true,
   },
   {
-    achievement:
-      "Halved the time in which a repair could be processed and completed as a Senior Technician at Stormfront Retail",
+    achievement: (
+      <>Graduated from Exeter University with a 2:1 BSc Physics (Hons.)</>
+    ),
     visible: true,
   },
   {
-    achievement:
-      "Gained 4 full A-Levels: A*, A, A, C, from Arthur Terry School and Sixth Form",
+    achievement: (
+      <>
+        Gained 4 full A-Levels: A*, A, A, C, from Arthur Terry School and Sixth
+        Form
+      </>
+    ),
     visible: true,
   },
 ];
+
 interface ListAchievementsProps {
   achievements: KeyAchievement[];
 }
 const ListAchievements: React.FC<ListAchievementsProps> = ({
   achievements,
-}) => (
-  <ul>
-    {achievements.map(
-      (achievement, achievementIndex) =>
-        achievement.visible && (
-          <React.Fragment key={achievementIndex}>
-            <li>{achievement.achievement}</li>
-            {achievement.subAchievements && (
-              <ListAchievements achievements={achievement.subAchievements} />
-            )}
-          </React.Fragment>
-        )
-    )}
-  </ul>
-);
+}) => {
+  if (!achievements.some((value) => value.visible)) {
+    return null;
+  }
+  return (
+    <ul>
+      {achievements.map(
+        (achievement, achievementIndex) =>
+          achievement.visible && (
+            <React.Fragment key={achievementIndex}>
+              <li>{achievement.achievement}</li>
+              {achievement.subAchievements && (
+                <ListAchievements achievements={achievement.subAchievements} />
+              )}
+            </React.Fragment>
+          )
+      )}
+    </ul>
+  );
+};
 
 /**
  * Summary of key achievements over recent years
@@ -162,14 +193,6 @@ const KeyAchievements: React.FC = () => (
   <section>
     <h3>Key Achievements</h3>
     <ListAchievements achievements={KEY_ACHIEVEMENTS} />
-    <ul>
-      {KEY_ACHIEVEMENTS.map(
-        (achievement, achievementIndex) =>
-          achievement.visible && (
-            <li key={achievementIndex}>{achievement.achievement}</li>
-          )
-      )}
-    </ul>
   </section>
 );
 // Key Achievements - END
@@ -189,6 +212,7 @@ type JobRole = {
   end: Date;
   // Roles in job
   roles: Position[];
+  rolesDescription: React.ReactNode;
   // Misc
   visible: boolean;
   link: string;
@@ -203,49 +227,84 @@ type Position = {
   start?: Date;
   /** If no end date, use parent end date */
   end?: Date;
-  description: string;
+  // description: string;
 };
 
 // TODO: Dates and Descriptions
 const PREVIOUS_ROLES: JobRole[] = [
   {
     companyName: "Select",
-    subTitle: "Formerly Stormfront Retail",
+    subTitle: "Formerly: Stormfront Retail",
     start: STARTED_STORMFRONT_RETAIL,
     end: STARTED_MTC,
-    // The positions held at Stormfront Retail were customer facing and required high attention to detail to diagnose issues with customers and their Apple devices efficiently. To improve repair times, I personally produced an internal training document for colleagues to refer to in the repair of customer devices, known as “The iOS Repair Handbook”.
     roles: [
       {
         positionName: "Team Member",
         end: new Date("2017-03-01"),
-        description: "Team Member",
       },
       {
         positionName: "Senior Technician",
         start: new Date("2017-03-01"),
-        description: "Senior Technician",
       },
     ],
+    rolesDescription: (
+      <p>
+        The positions held at Stormfront Retail were intensly customer facing
+        and required high attention to detail in order to efficiently diagnose
+        issues for customers with their Apple devices. In an effort to improve
+        repair times, I personally produced an internal training document for
+        colleagues to refer to; known as "The iOS Repair Handbook" it use lead
+        to a significant reduction in time taken to complete iPhone repairs and
+        replacements.
+      </p>
+    ),
     visible: true,
     link: "https://uk.selectonline.com/",
   },
   {
-    companyName: "The Manufacturing Technology Centre (MTC)",
+    companyName: "The Manufacturing Technology Centre",
+    subTitle: "The MTC",
     start: STARTED_MTC,
     end: STARTED_AVAMAE,
-    // During my time at The MTC, I have been involved in a number of projects, such as Encompass and Disman. My involvement in Encompass included leading the development of the web solution, which involved full-stack development using languages like Python and JavaScript. The solution is an Integrated Design Decision Support (IDDS) system that interfaced with databases and external applications. The Disman project also involved a web solution that was able to mine data from Inspection report documents and save them to a unified front-end that displayed analytics. While I have not directly developed Artificial Intelligence (AI) tools, I am keen to do so. I have also produced a Web-App template used in numerous projects including the “AI Ecosystem” - an internal demonstrator platform
     roles: [
       {
         positionName: "Graduate Research Engineer",
         end: new Date("2018-03-01"),
-        description: "GRE",
       },
       {
         positionName: "Research Engineer",
         start: new Date("2018-03-01"),
-        description: "RE",
       },
     ],
+    rolesDescription: (
+      <>
+        <p>
+          During my time at The MTC, I have been involved in a number of
+          projects, such as BluePlanet and Encompass
+        </p>
+        <p>
+          For the BluePlanet project, I architected an Industry 4.0, Internet of
+          Things (IoT) solution capable of remotely reporting telemetry data to
+          an AWS instance hosting ThingsBoard, a IoT dashboarding platform.
+        </p>
+        <p>
+          My involvement in Encompass included full-stack development with
+          Python, JavaScript, HTML, and CSS. I lead the design and development
+          of the Integrated Design Decision Support (IDDS) system that
+          interfaced with databases and external applications. I then had the
+          opportunity to deliver a presentation at EMO Hannover in 2019.
+        </p>
+        <p>
+          In addition to the aforementioned projects, I also produced a Python
+          Web-App template; it became widely used by the department in numerous
+          projects including the "AI Ecosystem" - an internal demonstrator
+          platform that compiled demonstrators showcasing the department's
+          capabilities and successes.
+        </p>
+      </>
+    ),
+    // Disman Project:
+    // The Disman project also involved a web solution that was able to mine data from Inspection report documents and save them to a unified front-end that displayed analytics.
     visible: true,
     link: "https://www.the-mtc.org/",
   },
@@ -256,9 +315,9 @@ const PREVIOUS_ROLES: JobRole[] = [
     roles: [
       {
         positionName: "App Engineer",
-        description: "App",
       },
     ],
+    rolesDescription: "",
     visible: true,
     link: "https://www.avamae.co.uk/",
   },
@@ -273,10 +332,11 @@ const PriorExperience: React.FC = () => (
     {PREVIOUS_ROLES.map(
       (job, jobIndex) =>
         job.visible && (
-          <div key={jobIndex} style={{ padding: "0.5rem 0" }}>
+          <div key={jobIndex}>
             <ResumeHeading
               heading={job.companyName}
               subTitle={job.subTitle}
+              link={job.link}
               start={job.start}
               end={job.end}
             />
@@ -288,9 +348,9 @@ const PriorExperience: React.FC = () => (
                   end={role.end ?? job.end}
                   smaller
                 />
-                <p>{role.description}</p>
               </React.Fragment>
             ))}
+            <p style={{ padding: "1rem 0rem" }}>{job.rolesDescription}</p>
           </div>
         )
     )}
@@ -345,7 +405,8 @@ const PAST_EDUCATION: EducationalInstitutions[] = [
       {
         title: "GCSE",
         notableGrades: [
-          "11 A-grades (including English, Maths, all Sciences, and ICT), 3 B-grades",
+          "11 A-grades (including English, Maths, all Sciences, and ICT)",
+          "3 B-grades",
         ],
       },
     ],
